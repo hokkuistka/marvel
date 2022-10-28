00fd3a4182485ec8fcc0b39a240436ddd7a46054
hokkuistka <73896279+hokkuistka@users.noreply.github.com>
2022-10-28 21:22:49 +0300

    Add files via upload

---
+++ dist/precache-manifest.57479d547d6fedc85abac141f9439a43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00612761c2146f0295c84c3e8df99c97",
+    "url": "/marvel/index.html"
+  },
+  {
+    "revision": "9b177f2ab139368feef2",
+    "url": "/marvel/js/app.7c4f99b2.js"
+  },
+  {
+    "revision": "30cef0fe2d42e864371c",
+    "url": "/marvel/js/chunk-vendors.358e40d0.js"
+  },
+  {
+    "revision": "7d847ac1b23eec3526716581798a8368",
+    "url": "/marvel/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/marvel/robots.txt"
+  }
+]);
