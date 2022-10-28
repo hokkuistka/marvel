28e69e026f5749d832a5684e68c97b1657927d5a
hokkuistka <73896279+hokkuistka@users.noreply.github.com>
2022-10-28 21:18:39 +0300

    Add files via upload

---
+++ precache-manifest.3f85f1e49c56a9f7d133e3a9f99d6682.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8252253dd58e88abb4279b0a92dd7690",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b6481a3e3cecdda0c11",
+    "url": "/js/app.a9c438fd.js"
+  },
+  {
+    "revision": "30cef0fe2d42e864371c",
+    "url": "/js/chunk-vendors.358e40d0.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
